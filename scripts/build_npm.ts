c2b3f48a50b8c3866965ea999ab2509d38192a99
qmotas <1974745+qmotas@users.noreply.github.com>
2022-07-27 21:01:50 +0900

    Feat/10 publish npm (#15)
    * npm用ビルドを追加
    
    * GitHub Actionでnpm publish
    
    * READMEにusageを追加

---
+++ scripts/build_npm.ts
@@ -0,0 +1,29 @@
+import { build, emptyDir } from "https://deno.land/x/dnt@0.28.0/mod.ts";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  importMap: "./import_map.json",
+  shims: {
+    deno: true,
+  },
+  package: {
+    name: "tree-fns",
+    version: Deno.args[0]?.replace(/^v/, ""),
+    description: "Simple utility functions for tree structure",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/qmotas/tree-fns.git",
+    },
+    bugs: {
+      url: "https://github.com/qmotas/tree-fns/issues",
+    },
+  },
+});
+
+// post build steps
+Deno.copyFileSync("LICENSE", "npm/LICENSE");
+Deno.copyFileSync("README.md", "npm/README.md");
